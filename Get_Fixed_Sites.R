84d6b45456224f16138f11a5f4117d0c41b6a66a
LukeAndersonTrocme <luke.anderson.trocme@gmail.com>
2017-11-19 10:24:29 -0500

    added parentheses to output file name

--- Get_Fixed_Sites.R
+++ Get_Fixed_Sites.R
@@ -20,18 +20,18 @@ join$NAG=1-join$X5.x
 join$JPT=1-join$X5.y
 
 MissingJPT<-join[which(join$NAG > 0.01 & join$JPT ==0),]
-MissingNAG<-join[which(join$JPT > 0.01 & join$NAG ==0),]	
+MissingNAG<-join[which(join$JPT > 0.01 & join$NAG ==0),]
 Remove<-join[which(join$NAG > 0.1 & join$JPT == 0| join$JPT > 0.1 & join$NAG == 0),]
 Remove0.01<-join[which(join$NAG > 0.01 & join$JPT == 0| join$JPT > 0.01 & join$NAG == 0),]
 Remove0.001<-join[which(join$NAG > 0.001 & join$JPT == 0| join$JPT > 0.001 & join$NAG == 0),]
 
 
 
-write.table(MissingJPT[,c(1,2)], file=paste(out,"MissingInJPT.txt",sep=""), quote=F, row.names=F, col.names=F)
-write.table(MissingNAG[,c(1,2)], paste(out,"MissingInNAG.txt",sep=""), quote=F, row.names=F, col.names=F)
-write.table(Remove[,c(1,2)], file=paste(out,"RemoveSites_0.1.txt",sep=""), quote=F, row.names=F, col.names=F, sep = "\t")
-write.table(Remove0.01[,c(1,2)], file=paste(out,"RemoveSites_0.01.txt",sep=""), quote=F, row.names=F, col.names=F, sep = "\t")
-write.table(Remove0.001[,c(1,2)], file=paste(out,"RemoveSites_0.001.txt",sep=""), quote=F, row.names=F, col.names=F, sep = "\t")
+write.table(MissingJPT[,c(1,2)], file=paste(out,"/MissingInJPT.txt",sep=""), quote=F, row.names=F, col.names=F)
+write.table(MissingNAG[,c(1,2)], paste(out,"/MissingInNAG.txt",sep=""), quote=F, row.names=F, col.names=F)
+write.table(Remove[,c(1,2)], file=paste(out,"/RemoveSites_0.1.txt",sep=""), quote=F, row.names=F, col.names=F, sep = "\t")
+write.table(Remove0.01[,c(1,2)], file=paste(out,"/RemoveSites_0.01.txt",sep=""), quote=F, row.names=F, col.names=F, sep = "\t")
+write.table(Remove0.001[,c(1,2)], file=paste(out,"/RemoveSites_0.001.txt",sep=""), quote=F, row.names=F, col.names=F, sep = "\t")
 
 
 #ggplot(data= MissingNAG, aes(x= MissingNAG $X5.x, y= MissingNAG $X5.y))+geom_bin2d(binwidth=c(0.006))+scale_fill_gradientn(limits=c(0,1000), breaks=seq(0, 1000, by=100), colours=rainbow(3))+theme_minimal()+xlab(label="NewJapanese")+ylab(label="100GenomeJapanese")+ggtitle("Site Frequency Spectrum of 1000GenomeJapanese and NewJapanese") + theme(plot.title = element_text(hjust = 0.5, size=20))
