c38f9362cf0f8746d1e39c12adbdb367b4d254b9
LukeAndersonTrocme <luke.anderson.trocme@gmail.com>
2017-11-28 12:17:28 -0500

    added git check last commit date/time

--- MetaPipe_generalized.sh
+++ MetaPipe_generalized.sh
@@ -27,14 +27,15 @@ fi
 
 cd /Users/luke/bin/smaller_mut_spectrum_pipeline
 
-PathToGenome="/Users/luke/genomes/genomes" #this is where the data is
-#PathToGenome="/Users/luke/genomes/genomes/test22" #this is where the data is
+#PathToGenome="/Users/luke/genomes/genomes" #this is where the data is
+PathToGenome="/Users/luke/genomes/genomes/test22" #this is where the data is
 pathToBed="/Users/luke/genomes/BED_MASKS" #this is where the bed masks are
 #check if folder exists
 mkdir -p /Users/luke/Documents/MutSpect/FilteredData/$TimeStamp
 #make a new directory for hour/minutes
 mkdir -p "/Users/luke/Documents/MutSpect/FilteredData/$TimeStamp/$chrom"
-outputDIR="/Users/luke/Documents/MutSpect/FilteredData/$TimeStamp/$chrom/$HourStamp" #set outputDIR
+#set outputDIR
+outputDIR="/Users/luke/Documents/MutSpect/FilteredData/$TimeStamp/$chrom/$HourStamp"
 mkdir -p $outputDIR
 mkdir -p $outputDIR/scripts
 #git rev-parse HEAD > $outputDIR/git_info.txt #(pointing to script directory)
@@ -48,9 +49,24 @@ cp PositionOfMutSpect.sh $outputDIR/scripts/
 cp FreqSpectPlot.R $outputDIR/scripts/
 cp PlotFilter.R $outputDIR/scripts/
 
-start=`date +%s` #timer
+echo "Time since last Git Commit"
+echo "MetaPipe_generalized.sh"
+git log -1 --format=%cd MetaPipe_generalized.sh
+echo "FiltrationPipeLine2.0.sh"
+git log -1 --format=%cd FiltrationPipeLine2.0.sh
+echo "MutSpect_PipeLine.sh"
+git log -1 --format=%cd MutSpect_PipeLine.sh
+echo "PositionOfMutSpect.sh"
+git log -1 --format=%cd PositionOfMutSpect.sh
+echo "FreqSpectPlot.R"
+git log -1 --format=%cd FreqSpectPlot.R
+echo "PlotFilter.R"
+git log -1 --format=%cd PlotFilter.R
 
+start=`date +%s` #timer
+startStep=`date +%s` #timer
 #Take inputVCF filter low qual sites
+
 if [ ! -f $outputDIR/NAG_chr$chrom.3bed_filtered.vcf.gz ]; then
   echo "########### 1 : Nag Filtration ###########"
   bash FiltrationPipeLine2.0.sh \
@@ -73,6 +89,10 @@ else echo "File Exists : $outputDIR/1kG_chr$chrom.3bed_filtered.vcf.gz "
 fi
 
 wait
+echo "Time to run Filtration pipeline on chr $chrom is : \
+$((($(date +%s)-$startStep)/60)) minutes or $((($(date +%s)-$startStep)/60/60)) hours"
+startStep=`date +%s` #timer
+
 #merge both files
 if [ ! -f $outputDIR/1kGenome_NAG_filtered_chr$chrom.vcf.gz ]; then
   echo "########### 3 : Merge Nag and 1000Genome ###########"
@@ -88,6 +108,11 @@ if [ ! -f $outputDIR/1kGenome_NAG_filtered_chr$chrom.vcf.gz ]; then
   $outputDIR/1kGenome_NAG_filtered_chr$chrom.vcf.gz
 else echo "File Exists : $outputDIR/1kGenome_NAG_filtered_chr$chrom.vcf.gz"
 fi
+
+echo "Time to run merging step on chr $chrom is : \
+$((($(date +%s)-$startStep)/60)) minutes or $((($(date +%s)-$startStep)/60/60)) hours"
+
+
 #run MutSpect PipeLine on first sample
 echo "########### MutSpect PipeLine ###########"
 bash MutSpect_PipeLine.sh \
@@ -98,6 +123,7 @@ $outputDIR \
 & echo "  #   #   #  MutSpect_PipeLine  Kill PID : $!"
 
 if [ ! -f $outputDIR/1000Genome_filtered_JUST_JPT_freq.frq ]; then
+  startStep=`date +%s` #timer
   echo "########### Get Fixed Sites ###########"
   vcftools \
   --gzvcf $outputDIR/1kGenome_NAG_filtered_chr$chrom.vcf.gz \
@@ -122,10 +148,14 @@ if [ ! -f $outputDIR/1000Genome_filtered_JUST_JPT_freq.frq ]; then
   $outputDIR/1000Genome_filtered_JUST_JPT_freq.frq \
   $outputDIR/NAGJapan_filtered_freq.frq \
   $outputDIR
+  echo "Time to run Get_Fixed_Sites step on chr $chrom is : \
+  $((($(date +%s)-$startStep)/60)) minutes or $((($(date +%s)-$startStep)/60/60)) hours"
+
 else echo "File Exists : $outputDIR/1000Genome_filtered_JUST_JPT_freq.frq "
 fi
 
 if [ ! -f $outputDIR/1kGenome_NAG_filtered_chr$chrom.RemoveSites_0.01.recode.vcf.gz ]; then
+  startStep=`date +%s` #timer
   echo "########### Exclude 0.01 Sites ###########"
   vcftools \
   --gzvcf $outputDIR/1kGenome_NAG_filtered_chr$chrom.vcf.gz \
@@ -136,6 +166,8 @@ if [ ! -f $outputDIR/1kGenome_NAG_filtered_chr$chrom.RemoveSites_0.01.recode.vcf
   && bgzip -f $outputDIR/1kGenome_NAG_filtered_chr$chrom.RemoveSites_0.01.recode.vcf \
   && tabix -f -p vcf $outputDIR/1kGenome_NAG_filtered_chr$chrom.RemoveSites_0.01.recode.vcf.gz #\
   #& echo "  #   #   #  Exclude 0.01 Sites  Kill PID : $!"
+  echo "Time to run Exclude 0.01 Sites step on chr $chrom is : \
+  $((($(date +%s)-$startStep)/60)) minutes or $((($(date +%s)-$startStep)/60/60)) hours"
 else echo "File Exists : $outputDIR/1kGenome_NAG_filtered_chr$chrom.RemoveSites_0.01.recode.vcf.gz"
 fi
 
@@ -159,6 +191,7 @@ $outputDIR \
 & echo "  #   #   #  MutSpect PipeLine (filtered 0.01)  Kill PID : $!"
 
 if [ ! -f $outputDIR/1000Genome_filtered0.01_JUST_JPT_freq.frq ]; then
+  startStep=`date +%s` #timer
   echo "########### SITE FREQUENCY SPECTRUM PLOT (filtered 0.01) ###########"
 
   vcftools \
@@ -178,7 +211,8 @@ else echo "File Exists : $outputDIR/1000Genome_filtered0.01_JUST_JPT_freq.frq"
 fi
 
 wait
-
+echo "Time to run SITE FREQUENCY SPECTRUM PLOT (filtered 0.01) step on chr $chrom is : \
+$((($(date +%s)-$startStep)/60)) minutes or $((($(date +%s)-$startStep)/60/60)) hours"
 echo "########### Position Of MutSpect (unfiltered)###########"
 bash PositionOfMutSpect.sh \
 $outputDIR/1kGenome_NAG_filtered_chr$chrom.vcf.gz \
