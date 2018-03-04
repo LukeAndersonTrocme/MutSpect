14d9ee924f2e1a6c0e4cec1688f19ddb0d81f4ea
LukeAndersonTrocme <luke.anderson.trocme@gmail.com>
2018-03-04 13:28:08 -0500

    added "remove N's from context"

--- AncestralContext.py
+++ AncestralContext.py
@@ -5,28 +5,32 @@ def main(args):
     #read input file (VCF file cut first 4 cols)
     vcfPos=pd.read_table(args.input,
                          names=["Chrom","Pos", "Ref", "Alt"])
-    #only keep SNPs
-    DNA=['A','C','G','T']
-    vcfPos= vcfPos[vcfPos['Ref'].isin(DNA)]
-    vcfPos= vcfPos[vcfPos['Alt'].isin(DNA)]
+
     #set default Derived Allele to 1
     vcfPos['DerivedAllele'] = 1
 
     #read chimp human alignment
-    chimp=pd.read_table('/Users/luke/bin/smaller_mut_spectrum_pipeline/\
-    hg19_chimp_align/human_chimp_diffs_chr' \
+    chimp=pd.read_table('/Users/luke/bin/smaller_mut_spectrum_pipeline/'
+    +'hg19_chimp_align/human_chimp_diffs_chr'
     + args.chrom + '.txt', sep=" ")
     chimp= chimp[chimp['SNP/Indel'] == 'SNP']
 
     #read oneLine fasta ref file
-    ref=open('/Users/luke/bin/smaller_mut_spectrum_pipeline/\
-    hg19_reference/chr' + args.chrom + '_oneline.txt')
+    ref=open('/Users/luke/bin/smaller_mut_spectrum_pipeline/'
+    +'hg19_reference/chr' + args.chrom + '_oneline.txt')
     refseq=ref.read()
 
     #get context (i.e. one base up- and downstream)
     vcfPos['Context'] = vcfPos.apply(\
     lambda row : refseq[row.Pos - 2 : row.Pos + 1], axis=1)
-
+    vcfPos=vcfPos.drop_duplicates(subset='Pos')
+    
+    #only keep SNPs
+    DNA=['A','C','G','T']
+    vcfPos= vcfPos[vcfPos['Ref'].isin(DNA)]
+    vcfPos= vcfPos[vcfPos['Alt'].isin(DNA)]
+    vcfPos= vcfPos[vcfPos['Context'].isin(DNA)]
+    
     #get sites shared by both files
     iPos = set(chimp['Pos']) & set(vcfPos['Pos'])
     iChimp = chimp[chimp['Pos'].isin(list(iPos))]
@@ -34,10 +38,12 @@ def main(args):
     assert len(iVCF) == len(iChimp)
     #find sites where Chimp is same as Human Alt
     ChimpDiff = iVCF[iVCF['Alt'] == list(iChimp['Chimp'])]['Pos']
+    #ChimpDiff = vcfPos['Pos'][vcfPos['Pos'].isin(chimp['Pos'])][vcfPos['Alt'] == list(chimp['Chimp'])]['Pos']
+    #print(ChimpDiff.head())
 
     ##deal with an error that happens for some chromosomes
     #ValueError: Arrays were different lengths: 321999 vs 321997
-    
+
     #For sites that have ChimpDiff, change DerivedAllele, Context, Ref and Alt
     vcfPos.loc[vcfPos['Pos'].isin(ChimpDiff), \
     'DerivedAllele'] = 0
@@ -53,7 +59,7 @@ def main(args):
 
     #write table
     vcfPos.to_csv(args.out + 'Chr' + args.chrom + \
-    '.AncestralContext.txt', index = False, sep="\t")
+    '.AncestralContext.txt', index = False, header = False, sep="\t")
 
 parser = argparse.ArgumentParser(description = 'get Ancestral Context')
 parser.add_argument('-input', help = 'input file')
