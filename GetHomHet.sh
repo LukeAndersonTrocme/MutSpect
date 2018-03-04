8c0ebef9228a16bf8b469524d6fe9cd07d7147c1
LukeAndersonTrocme <luke.anderson.trocme@gmail.com>
2018-03-04 13:28:46 -0500

    functional pipeline

--- GetHomHet.sh
+++ GetHomHet.sh
@@ -4,16 +4,35 @@ chrom=$2
 outputDIR=$3
 
 cd /Users/luke/bin/snpEff_latest_core/snpEff
+if [ -f $outputDIR/Chr$chrom.Genotypes.txt ]; then
+  echo "File $outputDIR/Chr$chrom.Genotypes.txt exists, no need to get GT"
+else
+  java -jar SnpSift.jar extractFields \
+  $myVcf POS "GEN[*].GT" \
+  | sed 's/\//|/g ; s/0|0/0/g ; s/0|1/1/g ; s/1|0/1/g ; s/1|1/2/g ; /|/d' \
+  | tail -n +2 > $outputDIR/Chr$chrom.Genotypes.txt
+fi
 
-# gzcat $myVcf | head -500 \
-# | grep "^#CHROM" \
-# | cut -f1,2,10- \
-# > $outputDIR/header.txt
+#check that the Genotype file doesn't have a header
+line=$(head -n 1 $outputDIR/Chr$chrom.Genotypes.txt)
+if [[ $line = *"POS"* ]]; \
+then echo "Have to remove fist line of Genotypes"
+tail -n +2 $outputDIR/Chr$chrom.Genotypes.txt > \
+tail -n +2 $outputDIR/Temp.Genotypes.txt && \
+mv $outputDIR/Temp.Genotypes.txt $outputDIR/Chr$chrom.Genotypes.txt
+fi
 
-java -jar SnpSift.jar extractFields \
-$myVcf CHROM POS "GEN[*].GT" \
-| sed 's/\//|/g' \
-| sed 's/0|0/0/g;s/0|1/1/g;s/1|0/1/g;s/1|1/2/g' \
-| sed '/|/d' \
+echo "#### Attempting Join"
+join -1 2 -2 1 -t $'\t' \
+$outputDIR/Chr$chrom.AncestralContext.txt \
+$outputDIR/Chr$chrom.Genotypes.txt \
+| awk 'BEGIN{ OFS = "\t" } { if ($5 == 0) {for (i = 7; i <= NF; i++) $i == $i - 2} print $0}' \
+| sed 's/-//g' \
 | sed "1s/.*/$(head -n 1 $outputDIR/header.txt )/" \
-| gzip -f > $outputDIR/Chr$chrom.Filtered_Genotypes.txt.gz
+| gzip > $outputDIR/Chr$chrom.FlippedGenotypes.Context.txt.gz
+
+gzcat $outputDIR/Chr$chrom.FlippedGenotypes.Context.txt.gz | wc -l
+
+echo "#### Attempted Join"
+#remove this file because it's big
+#rm $outputDIR/Chr$chrom.Genotypes.txt
